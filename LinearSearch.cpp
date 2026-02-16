ccb1710482b4065a7f0cc555d773e102559c8120
vaishnavi yelne <v16938372@gmail.com>
2026-02-16 06:57:27 +0530

    Update LinearSearch.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,31 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int linearSearch(int arr[], int n, int key, int index) {
-    if (index == n)
-        return -1;
-    if (arr[index] == key)
-        return index;
-    return linearSearch(arr, n, key, index + 1);
-}
+int main() 
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+    int key = 30;
+    int pos = -1;
 
-int main() {
-    int n, key;
-    cout << "Enter number of elements: ";
-    cin >> n;
+    for(int i = 0; i < 5; i++)
+    {
+        if(arr[i] == key)
+        {
+            pos = i;
+            break;
+        }
+    }
 
-    int arr[n];
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    cout << "Enter element to search: ";
-    cin >> key;
-
-    int result = linearSearch(arr, n, key, 0);
-
-    if (result != -1)
-        cout << "Element found at position: " << result + 1;
+    if(pos != -1)
+        cout << "Element found at index " << pos;
     else
         cout << "Element not found";
 
